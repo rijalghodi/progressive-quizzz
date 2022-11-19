dceb076d11604267e3abc622c39d4d6a1aa2d5c5
Rijal Ghodi <rijalghodi10@gmail.com>
2022-11-19 10:16:00 +0700

    quiz-navigation

--- components/quizNav/QuizNav.js
+++ components/quizNav/QuizNav.js
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./QuizNav.module.css";
 
-export default function QuizNav() {
+export default function QuizNav({ questions }) {
+  let number = 0;
   return (
     <aside className={styles.quizNav}>
-      <h2>Quiz Navigation</h2>
+      <h2 className="">Quiz Navigation</h2>
+      <div className="">
+        {questions?.map((question) => {
+          number++;
+          return (
+            <button
+              type="button"
+              title={`Go to Question no. ${number}`}
+              className={styles.questionNavButton}
+            >
+              <span>{number}</span>
+            </button>
+          );
+        })}
+      </div>
+      <div className="">
+        <button
+          type="button"
+          title={`Go to Question no. ${number}`}
+          className={styles.actionButton}
+        >
+          Submit
+        </button>
+      </div>
     </aside>
   );
 }
