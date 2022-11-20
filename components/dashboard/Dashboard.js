6c25ad5ce2478258ff4fda1d0440af08adae302c
Rijal Ghodi <rijalghodi10@gmail.com>
2022-11-20 16:03:02 +0700

    edit instruction

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -7,18 +7,17 @@ export default function Dashboard() {
       <main className="w-full sm:w-4/5 md:min-w-[550px] lg:w-1/2 py-32 px-6 lg:py-48  mx-auto">
         <article className="block mb-10">
           <h1 className="text-center mb-8">Welcome to Quizz</h1>
-          <p className="text-xl mb-3">
-            Here, you can test your color combination and save it into pallete
-            collection!
-          </p>
           <p className="text-xl">
-            This test follows the 60-30-10 rule to choose colors. Acording to
-            this rule, you just need three colors to style your web. 60 percent
-            of your web must be covered by the neutral color. Use it for
-            whitespaces and backgrounds. The next 30 percent is primary color,
-            by where you style texts and lines. And the last 10 percent is
-            accent color. It should be unique and user-attention attractor. It
-            is used for buttons and important texts.
+            Do you know what is the capital of indonesia? What is the flag color
+            of Italia? What is the biggest temple in the world? Wow. Geography
+            is interesting to learn. Test your knowledge about Geography here!
+            Geography!
+          </p>
+          <p className="text-xl mb-3">
+            There are 10 multiple questions about Geography. Choose the best
+            answer by clicking the A, B, C, or D. Submit your answers by
+            clicking the submit button on the last page or navigation bar.
+            Enjoy!
           </p>
         </article>
         <Link href={"/quiz"}>
