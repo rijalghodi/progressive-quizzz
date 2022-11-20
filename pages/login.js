6d3868e34f1084eee538366a898fb56ede15080f
Rijal Ghodi <rijalghodi10@gmail.com>
2022-11-20 14:47:02 +0700

    authentification folder case name

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
-import Login from "../components/authentification/Login";
+import Login from "../components/Authentification/Login";
 
 export default function LoginPage() {
   return (
