454638e39dee68bb03a6c1d71dcb55804d5e6f9b
Rijal Ghodi <rijalghodi10@gmail.com>
2022-11-21 12:23:05 +0700

    update firebase config

--- firebase.js
+++ firebase.js
@@ -11,16 +11,6 @@ const FirebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const FirebaseDevConfig = {
-  // Firebase auth api keys for development
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_DEV_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DEV_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_DEV_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_DEV_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_DEV_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_DEV_APP_ID,
-};
-
 const app = firebase.initializeApp(FirebaseConfig);
 
 export const auth = getAuth(app);
